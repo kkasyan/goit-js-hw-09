6c1d176a3a05757ea0456e27a315b08116caf100
kkasyan <104075234+kkasyan@users.noreply.github.com>
2022-10-08 07:50:27 +0000

    Deploying to gh-pages from @ kkasyan/goit-js-hw-09@adfca8023619a9716ca7ac368f2368fdb2a9c9e0 🚀

---
+++ 01-color-switcher.a38f99ab.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){o=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));n.style.backgroundColor=e,t.disabled=!0}),1e3)})),e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}));var o=null}();
+//# sourceMappingURL=01-color-switcher.a38f99ab.js.map
